5383bacccdc34712a978f3be93794230ca426e4a
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-21 13:51:10 +0530

    Added : test cases for calculating the batters with higest strike rate with 6s and 4s

--- src/test/java/com/capgemini/iplanalysertests/IPLAnalyserTest.java
+++ src/test/java/com/capgemini/iplanalysertests/IPLAnalyserTest.java
@@ -104,4 +104,18 @@ public class IPLAnalyserTest {
 		}
 	}
 
+	@Test
+	public void GivenBattingCSVFile_WhenLoaded_GivesHigestStrikeRateWith6sand4sBatsmen() {
+		try {
+			List<BattingDataCSV> list = iplAnalyser.getHigestStrikeRateWith6snad4sBatsmen(IPL_BATTING_DATA, 5);
+			for (BattingDataCSV p : list) {
+				System.out.println(p.playerName);
+			}
+			Assert.assertEquals("Andre Russell", list.get(0).playerName);
+		} catch (IPLAnalyserException e) {
+			fail();
+			e.printStackTrace();
+		}
+	}
+
 }

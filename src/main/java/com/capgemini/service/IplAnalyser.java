c874a6325ba9fd216f899b9d666278750d17c5ab
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-23 11:19:00 +0530

    Refactored : Added Comments to the functions in the ipl analyser class

--- src/main/java/com/capgemini/service/IplAnalyser.java
+++ src/main/java/com/capgemini/service/IplAnalyser.java
@@ -25,6 +25,9 @@ import com.capgemini.service.interfaces.IPLAnalyserBuilderInterface;
 
 public class IplAnalyser implements IPLAnalyserBuilderInterface {
 
+	/**
+	 * Generic Function to Load Pojo classes from csv file and return the list
+	 */
 	private <E> List<E> loadCSVData(String filePath, Class<E> classname) throws IPLAnalyserException {
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(filePath));
@@ -40,6 +43,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		}
 	}
 
+	/**
+	 * Function to remove the duplicate entries in the list and return a fresh list
+	 * of batsmen
+	 */
 	public List<IPLBatsman> loadCSVBattingData(String filePath, Class<IPLBatsman> classname)
 			throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVData(filePath, classname);
@@ -54,6 +61,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return newlist;
 	}
 
+	/**
+	 * Function to remove the duplicate entries in the list and return a fresh list
+	 * of bowlers
+	 */
 	public List<IPLBowler> loadCSVBowlingData(String filePath, Class<IPLBowler> classname) throws IPLAnalyserException {
 		List<IPLBowler> list = loadCSVData(filePath, classname);
 		Map<Integer, IPLBowler> hashmap = new HashMap<>();
@@ -67,30 +78,48 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return newlist;
 	}
 
+	/**
+	 * Function to return the list of batsmen having best average from the list
+	 * (UC1)
+	 */
 	public List<IPLBatsman> getTopAverageBatsmen(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
 		Comparator<IPLBatsman> comparator = Comparator.comparing(IPLBatsman::getAverage).reversed();
 		return getSortedList(list, comparator, topPlayers);
 	}
 
+	/**
+	 * Function to return the list of batsmen having best strike rate from the list
+	 * (UC2)
+	 */
 	public List<IPLBatsman> getTopStrikeRatedBatsmen(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
 		Comparator<IPLBatsman> comparator = Comparator.comparing(player -> player.strikeRate);
 		return getSortedList(list, Collections.reverseOrder(comparator), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of batsmen having most sixes from the list (UC3)
+	 */
 	public List<IPLBatsman> getTopSixHitterBatsmen(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
 		Comparator<IPLBatsman> comparator = Comparator.comparing(player -> player.sixes);
 		return getSortedList(list, Collections.reverseOrder(comparator), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of batsmen having most fours from the list (UC3)
+	 */
 	public List<IPLBatsman> getTopFourHitterBatsmen(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
 		Comparator<IPLBatsman> comparator = Comparator.comparing(player -> player.fours);
 		return getSortedList(list, Collections.reverseOrder(comparator), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of batsmen having best strike rate with sixes and
+	 * fours from the list (UC4)
+	 */
 	public List<IPLBatsman> getHigestStrikeRateWith6snad4sBatsmen(String filePath, int topPlayers)
 			throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
@@ -99,12 +128,20 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, comparator.reversed(), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of batsmen having best strike rate with average
+	 * from the list (UC5)
+	 */
 	public List<IPLBatsman> getBestAverageWithStrikeRate(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
 		Comparator<IPLBatsman> comparator = Comparator.comparing(p -> p.getAverage() * p.strikeRate);
 		return getSortedList(list, Collections.reverseOrder(comparator), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of batsmen having most runs with good average
+	 * from the list (UC6)
+	 */
 	public List<IPLBatsman> getHighestRunsWithAverage(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
 		Comparator<IPLBatsman> runcomparator = Comparator.comparing(p -> p.runs);
@@ -112,12 +149,20 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, Collections.reverseOrder(comparator), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of bowlers having best bowling average from the
+	 * list (UC7)
+	 */
 	public List<IPLBowler> getBestBowlingAveragePlayers(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBowler> list = loadCSVBowlingData(filePath, IPLBowler.class);
 		Comparator<IPLBowler> comparator = Comparator.comparing(p -> p.getAverage());
 		return getSortedList(list, comparator, topPlayers);
 	}
 
+	/**
+	 * Function to return the list of bowlers having best bowling strike rate from
+	 * the list (UC8)
+	 */
 	public List<IPLBowler> getBestBowlingStrikeRatePlayers(String filePath, int topPlayers)
 			throws IPLAnalyserException {
 		List<IPLBowler> list = loadCSVBowlingData(filePath, IPLBowler.class);
@@ -125,6 +170,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, comparator, topPlayers);
 	}
 
+	/**
+	 * Function to return the list of bowlers having best bowling economy rate from
+	 * the list (UC9)
+	 */
 	public List<IPLBowler> getBestBowlingEconomyRatePlayers(String filePath, int topPlayers)
 			throws IPLAnalyserException {
 		List<IPLBowler> list = loadCSVBowlingData(filePath, IPLBowler.class);
@@ -132,6 +181,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, comparator, topPlayers);
 	}
 
+	/**
+	 * Function to return the list of bowlers having most 5w or 4w with good strike
+	 * rate from the list (UC10)
+	 */
 	public List<IPLBowler> getBestStrikeRateWith4wAnd5w(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBowler> list = loadCSVBowlingData(filePath, IPLBowler.class);
 		Comparator<IPLBowler> strikeRateComparator = Comparator.comparing(p -> p.getStrikeRate());
@@ -140,6 +193,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, comparator, topPlayers);
 	}
 
+	/**
+	 * Function to return the list of bowlers having best bowling average with
+	 * strike rate from the list (UC11)
+	 */
 	public List<IPLBowler> getBestAverageWithStrikeRateBowlers(String filePath, int topPlayers)
 			throws IPLAnalyserException {
 		List<IPLBowler> list = loadCSVBowlingData(filePath, IPLBowler.class);
@@ -147,6 +204,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, comparator, topPlayers);
 	}
 
+	/**
+	 * Function to return the list of bowlers having most wickets with best average
+	 * from the list (UC12)
+	 */
 	public List<IPLBowler> getBestWicketsWithBestAverageBowlers(String filePath, int topPlayers)
 			throws IPLAnalyserException {
 		List<IPLBowler> list = loadCSVBowlingData(filePath, IPLBowler.class);
@@ -156,6 +217,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, comparator, topPlayers);
 	}
 
+	/**
+	 * Function to return the list of allrounders having best batting average and
+	 * bowling average from the list (UC13)
+	 */
 	public List<IPLAllrounder> getBestBattingAndBowlingAveragePlayers(String filePathBatting, String filePathBowling,
 			int topPlayers) throws IPLAnalyserException {
 		List<IPLAllrounder> allRouders = getAllRoundersList(filePathBatting, filePathBowling);
@@ -163,6 +228,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(allRouders, comparator.reversed(), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of allrounders having most wickets and runs from
+	 * the list (UC14)
+	 */
 	public List<IPLAllrounder> getMostRunsAndWicketGettingPlayers(String filePathBatting, String filePathBowling,
 			int topPlayers) throws IPLAnalyserException {
 		List<IPLAllrounder> allRouders = getAllRoundersList(filePathBatting, filePathBowling);
@@ -170,6 +239,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(allRouders, comparator.reversed(), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of batsmen having most hundreds with best average
+	 * from the list (UC15)
+	 */
 	public List<IPLBatsman> getHighestHundredsWithAverage(String filePath, int topPlayers) throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
 		Comparator<IPLBatsman> hundredcomparator = Comparator.comparing(p -> p.hundreds);
@@ -178,6 +251,10 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, comparator.reversed(), topPlayers);
 	}
 
+	/**
+	 * Function to return the list of batsmen having zero hundreds or fifties with
+	 * best average from the list (UC16)
+	 */
 	public List<IPLBatsman> getZeroHundredsOrFiftiesWithGoodAverageBatsmen(String filePath, int topPlayers)
 			throws IPLAnalyserException {
 		List<IPLBatsman> list = loadCSVBattingData(filePath, IPLBatsman.class);
@@ -187,15 +264,25 @@ public class IplAnalyser implements IPLAnalyserBuilderInterface {
 		return getSortedList(list, avgComparator, topPlayers);
 	}
 
+	/**
+	 * Generic Function to return the sorted list of a fixed size by a comparator
+	 */
 	private <E> List<E> getSortedList(List<E> list, Comparator<E> comparator, int topPlayers) {
 		return list.stream().sorted(comparator).limit(topPlayers).collect(Collectors.toList());
 	}
 
+	/**
+	 * Function to check if a given name available in the bowlers list, if available
+	 * returns the bowler
+	 */
 	private IPLBowler isAvailableInBowlers(String playerName, List<IPLBowler> bowlerList) {
 		Predicate<IPLBowler> isAvailableInList = player -> player.playerName.equalsIgnoreCase(playerName);
 		return bowlerList.stream().filter(isAvailableInList).findFirst().orElse(null);
 	}
 
+	/**
+	 * Function to return the all rounder list from batsmen and bowlers list
+	 */
 	private List<IPLAllrounder> getAllRoundersList(String filePathBatting, String filePathBowling)
 			throws IPLAnalyserException {
 		List<IPLBatsman> batsmenList = loadCSVBattingData(filePathBatting, IPLBatsman.class);
